b4bf65fa5c7a4ab38fc3a880835bf946362a4ba3
bilbo <bilbo@ubimate.com>
2018-02-27 23:16:39 +0100

    changed copyright attribution

--- src/ub1/core/Body.hx
+++ src/ub1/core/Body.hx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Fabrizio Capolini
+ * Copyright (c) 2018 Ubimate.com and Ub1 contributors.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
